73e90ff2af5848fec7427f3ae758949c8f7fae34
SansEsriff <obni12@inbox.lv>
2023-02-06 16:00:09 +0300

    cosmetic changes

--- test_items.py
+++ test_items.py
@@ -8,4 +8,4 @@ def test_user_see_add_to_basket_button(browser):
     browser.implicitly_wait(20)
     browser.get(link)
     button = browser.find_element(By.CSS_SELECTOR, 'button.btn.btn-add-to-basket')
-    assert button, 'Button "Add to basket" not found'
+    assert button, 'Button to add item in basket not found'
